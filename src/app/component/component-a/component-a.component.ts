9b41db6c11c1fc90920e7e16b0614bfc25ad3f68
hoacodai000 <trungvq94@gmail.com>
2020-03-09 21:57:33 +0700

    send and sub data

--- src/app/component/component-a/component-a.component.ts
+++ src/app/component/component-a/component-a.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { BroadcasterService } from '../../services/broadcaster.service';
+import { User } from '../../models/user';
 
 @Component({
   selector: 'app-component-a',
@@ -7,10 +9,14 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ComponentAComponent implements OnInit {
-
-  constructor() { }
+  public user: User = { id: 1, firstName: 'Trung', lastName: 'Vo', role: 'Admin' };
+  constructor(private BroadcasterService: BroadcasterService) { }
 
   ngOnInit(): void {
+  }
+
+  sendDataFromComponentA(): void {
+    this.BroadcasterService.broadcast('DATA_FROM_COMPONENT_A', this.user);
   }
 
 }
--- src/app/component/component-b/component-b.component.ts
+++ src/app/component/component-b/component-b.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, OnDestroy, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
+import { BroadcasterService } from '../../services/broadcaster.service';
+import { User } from '../../models/user';
+
+import { SubSink } from '../../helpers/subsink';
 
 @Component({
   selector: 'app-component-b',
@@ -6,11 +10,26 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
   styleUrls: ['./component-b.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ComponentBComponent implements OnInit {
+export class ComponentBComponent implements OnInit, OnDestroy {
+  public user: User;
+  private subs = new SubSink();
 
-  constructor() { }
+  constructor(
+    private broadcasterService: BroadcasterService,
+    private changeDetectorRef: ChangeDetectorRef) { }
 
   ngOnInit(): void {
+    this.subscriptionData();
   }
 
+  ngOnDestroy(): void {
+    this.subs.unsubscribe();
+  }
+
+  subscriptionData(): void {
+    this.subs.sink = this.broadcasterService.on('DATA_FROM_COMPONENT_A').subscribe((user: User) => {
+      this.user = user;
+      this.changeDetectorRef.detectChanges();
+    });
+  }
 }
